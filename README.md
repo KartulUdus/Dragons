40f6d991eacc5afcfece81c05084119abe3e1fe7
Derek Mäekask <derek.maekask@myjar.com>
2017-09-12 14:36:16 +0300

    Minor text fixes and readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Dragons
-giving this mugloar thing a shot
+This is my solution to the RESTFUL dragon game https://www.dragonsofmugloar.com/
+
+Anyone checking this from BigBank, I'm not looking for a job at this time.
--- charizard.py
+++ charizard.py
@@ -8,13 +8,13 @@ import heapq
 
 getGame = 'http://www.dragonsofmugloar.com/api/game'
 
+
 def getweather(id):
 
-    wurl=('http://www.dragonsofmugloar.com/weather/api/report/'+str(id))
+    wurl = ('http://www.dragonsofmugloar.com/weather/api/report/' + str(id))
     potato = requests.get(wurl).content
     weather = json.loads(json.dumps(xmltodict.parse(potato)))
     return weather['report']
-
 
 
 def potato():
@@ -26,8 +26,8 @@ def potato():
     if 'name' in ash:
         del ash['name']
     print "==========================="
-    print ("KNIGHT:",ash)
-    print ("WEATHER:",getweather(id)['code'])
+    print ("KNIGHT:", ash)
+    print ("WEATHER:", getweather(id)['code'])
 
     ash['wingStrength'] = ash.pop('agility')
     ash['scaleThickness'] = ash.pop('attack')
@@ -36,32 +36,33 @@ def potato():
 
     stats = heapq.nlargest(4, ash, key=ash.get)
 
-    ash[stats[0]] +=2
-    ash[stats[1]] -=1
-    ash[stats[2]] -=1
+    ash[stats[0]] += 2
+    ash[stats[1]] -= 1
+    ash[stats[2]] -= 1
 
-
-    if (getweather(id)['code']=='NMR'):
+    if (getweather(id)['code'] == 'NMR'):
         charizard = genCharizard(ash)
-        print ("DRAGON:",charizard['dragon'])
-        tobattle(charizard,id)
+        print ("DRAGON:", charizard['dragon'])
+        tobattle(charizard, id)
     else:
         print "The weather is not nice, i'd rather stay home"
 
+
 def play():
 
-    count=40
+    count = 40
     for games in range(count):
         potato()
 
 
-def tobattle (dragon, id):
+def tobattle(dragon, id):
 
     url = 'http://www.dragonsofmugloar.com/api/game/' + str(id) + '/solution'
-    charizard=json.dumps(dragon)
+    charizard = json.dumps(dragon)
     headers = {"Content-Type": "application/json"}
-    result=requests.put(url,data=charizard,headers=headers).content
-    print ("RESULT:",json.loads(result))
+    result = requests.put(url, data=charizard, headers=headers).content
+    print ("RESULT:", json.loads(result))
+
 
 def genCharizard(dragon):
 
@@ -70,5 +71,6 @@ def genCharizard(dragon):
     }
     return charizard
 
+
 if __name__ == "__main__":
-  play()
+    play()
