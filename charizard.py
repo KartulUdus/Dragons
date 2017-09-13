6a5f79b7a0086025c3e32748c831c20a3e8b4cc3
Derek Mäekask <derek.maekask@myjar.com>
2017-09-13 21:52:41 +0300

    maybe now

--- charizard.py
+++ charizard.py
@@ -6,7 +6,6 @@ import xmltodict
 import heapq
 
 
-
 def getweather(id):
 
     wurl = ('http://www.dragonsofmugloar.com/weather/api/report/' + str(id))
@@ -17,46 +16,75 @@ def getweather(id):
 
 def trainCharizard(ash, weather):
 
-    if 'name' in ash:
-        del ash['name']
+
     ash['wingStrength'] = ash.pop('agility')
     ash['scaleThickness'] = ash.pop('attack')
     ash['clawSharpness'] = ash.pop('armor')
     ash['fireBreath'] = ash.pop('endurance')
 
+    if 'name' in ash:
+        del ash['name']
+
+
     stats = heapq.nlargest(4, ash, key=ash.get)
 
-    ash[stats[0]] += 2
-    ash[stats[1]] -= 1
-    ash[stats[2]] -= 1
+    if weather in ("NMR", "FUNDEFINEDG"):
 
-    charizard = genCharizard(ash)
+        ash[stats[0]] += 2
+        ash[stats[1]] -= 1
+        ash[stats[2]] -= 1
+
+    if weather == "T E":
+
+        ash['wingStrength'] = 5
+        ash['scaleThickness'] = 5
+        ash['clawSharpness'] = 5
+        ash['fireBreath'] = 5
+
+    if weather == "HVA":
+
+        ash['wingStrength'] = 6
+        ash['scaleThickness'] = 6
+        ash['clawSharpness'] = 8
+        ash['fireBreath'] = 0
+
+
+    charizard = {
+        "dragon": ash
+    }
+
 
     print ("DRAGON: ", charizard)
+
+
     print ("DEBUG WEATHER: ", weather)
 
     return charizard
 
 def potato():
 
-    getGame = 'http://www.dragonsofmugloar.com/api/game'
-    game = json.loads(requests.get(getGame).content)
+    getGame = requests.get('http://www.dragonsofmugloar.com/api/game').text
+    game = json.loads(getGame)
     ash = game['knight']
     id = game['gameId']
     weather = getweather(id)
 
-    print ("=========================== GAME: ",game)
+    print ("===========================")
+    if weather['code']=="HVA":
+        print ash['endurance']
     print ("KNIGHT:", ash)
-    print ("WEATHER:", weather)
+#    print ("WEATHER:", weather)
+    charizard = trainCharizard(ash, weather['code'])
+    if weather['code'] is not "SRO":
+        tobattle(charizard, id)
+    else:
+        print "I WANT TO LIVE!!"
 
-    charizard = trainCharizard(ash, weather)
-    tobattle(charizard, id)
 
-    exit(1)
 
 def play():
 
-    count = 4
+    count = 400
     for games in range(count):
         potato()
 
@@ -71,13 +99,5 @@ def tobattle(dragon, id):
     print ("RESULT:", result)
 
 
-def genCharizard(dragon):
-
-    charizard = {
-        "dragon": dragon
-    }
-    return charizard
-
-
 if __name__ == "__main__":
-    potato()
+    play()
